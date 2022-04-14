ef750970e260dd3419a635b4777b5c46d3d7982d
flsantos <flsantos@apdata.com.br>
2022-04-14 16:07:27 -0300

    add: Desenvolvendo o jogo da cobrinha

--- code.js
+++ code.js
@@ -1,4 +1,17 @@
 let ctx = document.getElementById("canvas").getContext("2d")
-ctx.fillStyle = "red"
-ctx.arc(10, 590, 5, 0, 2 * Math.PI)
-ctx.fill();
+const size = 5
+
+function draw(x, y, size) {
+  ctx.fillStyle = "red"
+  ctx.arc(x, y, size, 0, 2 * Math.PI)
+  ctx.fill();
+}
+
+
+function mainLoop() {
+  //draw(Math.random() * window.innerWidth, Math.random() * window.innerHeight, Math.random() * 100)
+  draw(5, 5, size);
+  requestAnimationFrame(mainLoop)
+}
+
+mainLoop()
